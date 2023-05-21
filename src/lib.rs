9822b1503b8edd96f6a37c224137bac7f97c9ddc
Miccah Castorina <m.castorina93@gmail.com>
2023-05-20 20:37:47 -0500

    Implement XorCycler and xor_cycle method

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod tests {
     use super::b64::*;
     use super::freq::*;
     use super::hex::*;
+    use super::xor::*;
     use super::*;
     use std::iter;
 
@@ -85,7 +86,7 @@ mod tests {
         let message = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 
         assert_eq!(
-            xor::bytewise(message.bytes(), "ICE".bytes().cycle()).hex_encode().collect::<String>(),
+            message.bytes().xor_cycle(b"ICE").hex_encode().collect::<String>(),
             "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
         );
     }
--- src/xor.rs
+++ src/xor.rs
@@ -1,4 +1,5 @@
 use std::borrow::Borrow;
+use std::iter::Cycle;
 
 // Performs a byte-wise xor of two u8 iterators.
 pub fn bytewise<A, B>(a: A, b: B) -> impl Iterator<Item = u8>
@@ -12,3 +13,46 @@ where
         .zip(b.into_iter())
         .map(|(a, b)| a.borrow() ^ b.borrow())
 }
+
+// An iterator to emit xor bytes of two iterators.
+pub struct XorCycler<I, J>
+where
+    I: Iterator,
+    J: Iterator,
+{
+    upstream: I,
+    key: J,
+}
+
+// Implement Iterator trait for XorCycler.
+impl<I, J> Iterator for XorCycler<I, J>
+where
+    I: Iterator,
+    I::Item: Borrow<u8>,
+    J: Iterator,
+    J::Item: Borrow<u8>,
+{
+    type Item = u8;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.upstream.next()?.borrow() ^ self.key.next()?.borrow())
+    }
+}
+
+// Trait extension to add xor_cycle method to any iterator.
+pub trait XorCyclerExt: Iterator {
+    fn xor_cycle<I>(self, key: I) -> XorCycler<Self, Cycle<<I as IntoIterator>::IntoIter>>
+    where
+        Self: Sized,
+        I: IntoIterator,
+        <I as IntoIterator>::IntoIter: Clone,
+        <I as IntoIterator>::Item: Borrow<u8>,
+    {
+        XorCycler {
+            upstream: self,
+            key: key.into_iter().cycle(),
+        }
+    }
+}
+
+impl<I: Iterator> XorCyclerExt for I {}
